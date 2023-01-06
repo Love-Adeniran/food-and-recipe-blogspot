79b43cb51a6ff54009dc666a27ba1176095be493
Love <loveadeniran1@gmail.com>
2023-01-06 17:54:10 +0100

    validation for the signup and login forms

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const express = require("express")
 const app = express()
+const ejs = require("ejs")
 app.set("view engine","ejs")
 const path = require("path")
 
 // app.use('/images', express.static(__dirname + 'views/images'))
-
+app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")))
 
-let allPost =[]
-let eachPost = {
-    
-}
+let allUser =[]
+let allPost = []
 
 app.get('/',(req,res)=>{
     res.render("index")
 })
+
+//for signUp page
 app.get('/signup',(req,res)=>{
     res.render('signUp')
 })
-app.get('/login',(req,res)=>{
-    res.render('login')
+app.post('/signUp',(req,res)=>{
+    allUser.push(req.body)
+    console.log(allUser)
+    res.redirect("/login")
 })
-app.get('/home',(req,res)=>{
+
+//for login page
+app.get('/login',(req,res)=>{
+    res.render('login',{allUser})
+})
+app.post('/login',(req,res)=>{
+    for ( i = 0; i < allUser.length; index++) {
+        let newLogin = req.body
+        if (newLogin.email === allUser[i].email && newLogin.password === allUser[i].password) {
+            // console.log()
+           res.redirect("/home") 
+        }
+        else{
+            alert('Opppsssss! Invalid Email or Password!')
+        }
+    }
+})
+
+// for the blogspot posts
+app.get('/home',(req,res)=>{ 
+    
     res.render('home')
+})
+app.post('/home',()=>{
+    
 })
  
 // app.get('/',(req,res)=>{
